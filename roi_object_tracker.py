03e9b80517923c5be3c9e6d2ce7df2f372d1bfa3
malo <vasum@lafayette.edu>
2021-04-28 20:00:16 -0400

    major typo

--- roi_object_tracker.py
+++ roi_object_tracker.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 if len(physical_devices) > 0:
-    tf.config.experimental.set_memory_agrowth(physical_devices[0], True)
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
 import core.utils as utils
 from core.yolov4 import filter_boxes
 from tensorflow.python.saved_model import tag_constants
